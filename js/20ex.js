5969e325602487cbda0da85769f03cb2c1fed0ad
kimsohee <koreait.sec2020@gmail.com>
2025-06-26 09:58:59 +0900

    js day5

---
+++ js/20ex.js
@@ -0,0 +1,75 @@
+const time = ['09:00', '11:00', '12:30', '14:00', '16:45']
+const todo = ['수업', '과제', '점심식사', '주간회의', '자료조사']
+
+/*
+    위의 2개의 배열을 객체 배열 1개로 변경.
+    객체 예시 : {time: '09:00', todo: '과제'}
+*/
+// 비어있는 배열에 새로운 요소를 추가 : push() 메소드 활용
+const schedule = []
+for (let i = 0; i < time.length; i++) {
+  let newItem = { time: time[i], todo: todo[i] }
+  // 속성이름: 값
+  schedule.push(newItem)
+}
+
+/*
+const schedule = [
+  { time: '09:00', todo: '수업' },
+  { time: '11:00', todo: '과제' },
+  { time: '12:30', todo: '점심식사' },
+  { time: '14:00', todo: '주간회의' },
+  { time: '16:45', todo: '자료조사' }
+]
+*/
+
+// schedule.forEach() 로 연습하기
+
+function printRow(item) {        // '하나씩 가져온' 요소를 item 변수에 저장-> tr 요소를 만들기 
+  const tr = document.createElement('tr')
+  for (let key in item) {    //객체 모든 속성을 순회
+    // console.log(item[key])
+    const td = document.createElement('td')
+    td.textContent = item[key]
+    tr.appendChild(td)
+  }
+
+  return tr
+}
+
+const newBtn = document.getElementById('newBtn')
+newBtn.addEventListener('click', () => {
+  // 입력값 가져와서 newItem 객체 만들기
+
+  // printRow 실행하여 tr 전달 받기
+
+  // tr을 table에 추가
+
+})
+
+
+
+
+// DOMContentLoaded 이벤트 : DOM 자료구조를 만들어서 메모리에 저장한 후에 발생.
+//              ㄴ 화면의 구성 요소들이 만들어진 후에 실행.
+
+document.addEventListener('DOMContentLoaded', function () {
+  // html 의 최상위 요소 가져오기
+  const root = document.getElementById('root')
+  // table 요소를 만들어서 첫번째 줄(tr)에 제목을 넣어 table에 추가
+  const table = document.createElement('table')
+  table.innerHTML = `<tr>
+        <th>이름</th>
+        <th>나이</th>
+        </tr>`
+  // table 을 root 요소에 추가하기        
+  root.appendChild(table)
+
+  // 배열에 저장된 객체 요소 각각에 대해 반복하는 메소드
+  schedule.forEach(item => {
+    // item 객체로 tr요소 만드는 함수 호출
+    const trResult = printRow(item)
+    // console.log(trResult)
+    table.appendChild(trResult)
+  })
+})
